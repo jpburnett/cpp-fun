26c0e63d0c514ac76a1e9fe95c39d096755cd4b7
Parker Burnett <burnettparker@gmail.com>
2018-10-27 23:41:15 -0600

    Finish the sleep and print functions

--- circuit_problem/README.md
+++ circuit_problem/README.md
@@ -1,3 +1,15 @@
 # circuit_problem
 
-Just a fun little project to draw a simple resistor circuit and do the math for it
+Just a fun little project to draw a simple resistor circuit and do the math for it.
+This program prints out a simple two resistor circuit. I will eventually add functionality to solve the circuit.
+Purpose of this program will be eventually to be an educational reference about the math behind circuits, good ol' `V = IR`
+
+```C++
+  ______/\/\/\_______
+  |       ? Ω       |
+  |                 |
+(+ -)              ? Ω
+  10V               |
+  |                 |
+  |_________________|
+  ```
--- circuit_problem/circuit_problem/main.cpp
+++ circuit_problem/circuit_problem/main.cpp
@@ -5,36 +5,48 @@
 //  Created by Parker Burnett on 10/27/18.
 //  Copyright © 2018 Parker Burnett. All rights reserved.
 //
+#include <time.h>
 
 #include <iostream>
 
 
 using namespace std;
 
-void printCircuit() {
-    cout << "  ______/\\/\\/\\______+" << endl;
-    cout << "  |       75Ω       |\n";
-    cout << "  |                 Ω\n";
-    cout << "(+ -)\n";
-    cout << "  |\n";
-    cout << "  |\n";
-    cout << "  |_______________\n";
+// Function to sleep and delay the output for a second
+void sleep(uint sleeptime){
+    clock_t goal = sleeptime + clock();
+    while (goal > clock());
+}
+
+// Prints the layout of a circuit board
+void printCircuit(int r_val1, int r_val2) {
+    cout << "  ______/\\/\\/\\_______" << endl;
+    cout << "  |     " << r_val1 << " Ω       |\n";
+    cout << "  |                 |\n";
+    cout << "(+ -)             "<< r_val2 <<" Ω\n";
+    cout << " 10V                |\n";
+    cout << "  |                 |\n";
+    cout << "  |_________________|\n";
 }
 
 int main() {
-    // insert code here...
-    cout << "Printing Circuit:\n";
-    printCircuit();
+    // Initialize values for the resistors
+    int r1 = 0, r2 = 0;
     
-    clock_t start;
-    int pause = 10000;
+    cout << "Please enter value for R1: ";
+    cin >> r1;
+    cout << "Please enter value for R2: ";
+    cin >> r2;
     
-    for(int i = 0; i < 3; i++)
-    {
-        cout<< " . " << flush;
-        start = clock();
-        while( clock() < start + pause );
-    }
+    cout << "Printing Circuit";
+    sleep(1200000);
+    cout << ".";
+    sleep(1200000);
+    cout << ".";
+    sleep(1200000);
+    cout << "." << "\n";
+    sleep(1200000);
+    printCircuit(r1, r2);
     
     return 0;
 }
